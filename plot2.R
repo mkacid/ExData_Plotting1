9b6d05cbc7b80f7f70cdd355e34520968f6b0dd7
Kaci Moore <mkacid@okstate.edu>
2016-02-07 22:25:13 -0600

    Formatting issues

--- plot2.R
+++ plot2.R
@@ -2,7 +2,9 @@
 #   2007-02-02
 
 # Reads in the Household Power Consumption data from the text file
-epc <- read.table("household_power_consumption.txt",header=T,sep=";",colClasses=c("character","character","double","double","double","double","double","double","numeric"),na.strings="?")
+epc <- read.table("household_power_consumption.txt",header=T,sep=";",colClasses=
+    c("character","character","double","double","double","double","double",
+    "double","numeric"),na.strings="?")
 
 # Formats the date and time columns to be a single date/time class column
 epc$DateTime = paste(epc$Date, epc$Time)
@@ -11,10 +13,13 @@ epc$Date = NULL
 epc$Time = NULL
 
 # Gets the relevant data needed for analysis (2007-02-01 and 2007-02-02)
-subepc <- subset(epc,DateTime$year==107 & DateTime$mon==1 & (DateTime$mday==1 | DateTime$mday==2))
+subepc <- subset(epc,DateTime$year==107 & DateTime$mon==1 & (DateTime$mday==1 | 
+    DateTime$mday==2))
 
 #Plots Global Active Power with respect to time in png file
 png("plot2.png")
-plot(x=(subepc$DateTime),y=subepc$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+plot(x=(subepc$DateTime),y=subepc$Global_active_power,type="l",ylab="Global 
+    Active Power (kilowatts)",xlab="")
+
 dev.off()
 
--- plot3.R
+++ plot3.R
@@ -1,7 +1,9 @@
 # Plots the values of Energy sub metering for 2007-02-01 and 2007-02-02
 
 # Reads in the Household Power Consumption data from the text file
-epc <- read.table("household_power_consumption.txt",header=T,sep=";",colClasses=c("character","character","double","double","double","double","double","double","numeric"),na.strings="?")
+epc <- read.table("household_power_consumption.txt",header=T,sep=";",colClasses=
+    c("character","character","double","double","double","double","double",
+    "double","numeric"),na.strings="?")
 
 # Formats the date and time columns to be a single date/time class column
 epc$DateTime = paste(epc$Date, epc$Time)
@@ -10,12 +12,16 @@ epc$Date = NULL
 epc$Time = NULL
 
 # Gets the relevant data needed for analysis (2007-02-01 and 2007-02-02)
-subepc <- subset(epc,DateTime$year==107 & DateTime$mon==1 & (DateTime$mday==1 | DateTime$mday==2))
+subepc <- subset(epc,DateTime$year==107 & DateTime$mon==1 & (DateTime$mday==1 | 
+    DateTime$mday==2))
 
 #Plots Energy sub-metering over time in png file
 png("plot3.png")
-plot(x=(subepc$DateTime),y=subepc$Sub_metering_1,type="l",ylab="Energy sub metering",xlab="")
+plot(x=(subepc$DateTime),y=subepc$Sub_metering_1,type="l",ylab="Energy sub 
+    metering",xlab="")
 lines(x=(subepc$DateTime),y=subepc$Sub_metering_2,col="red")
 lines(x=(subepc$DateTime),y=subepc$Sub_metering_3,col="blue")
-legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="l",col=c("black","red","blue"),lwd=2,cex=0.7)
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="l",
+    col=c("black","red","blue"),lwd=2,cex=0.7)
+
 dev.off()
