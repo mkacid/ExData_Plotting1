b884873010cf78832cfc26b4eb12168da4053b3c
Kaci Moore <mkacid@okstate.edu>
2016-02-07 22:14:39 -0600

    Finished assignment

---
+++ plot4.R
@@ -0,0 +1,32 @@
+# Plots the values of Energy sub metering for 2007-02-01 and 2007-02-02
+
+# Reads in the Household Power Consumption data from the text file
+epc <- read.table("household_power_consumption.txt",header=T,sep=";",colClasses=c("character","character","double","double","double","double","double","double","numeric"),na.strings="?")
+
+# Formats the date and time columns to be a single date/time class column
+epc$DateTime = paste(epc$Date, epc$Time)
+epc$DateTime = as.POSIXlt(epc$DateTime,format="%d/%m/%Y %H:%M:%S")
+epc$Date = NULL
+epc$Time = NULL
+
+# Gets the relevant data needed for analysis (2007-02-01 and 2007-02-02)
+subepc <- subset(epc,DateTime$year==107 & DateTime$mon==1 & (DateTime$mday==1 | DateTime$mday==2))
+
+#Plots Energy sub-metering over time in png file
+env <- par()
+
+
+png("plot4.png")
+par(mfrow=c(2,2))
+plot(x=(subepc$DateTime),y=subepc$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+
+plot(x=(subepc$DateTime),y=subepc$Voltage,type="l",ylab="Voltage",xlab="datetime")
+
+plot(x=(subepc$DateTime),y=subepc$Sub_metering_1,type="l",ylab="Energy sub metering",xlab="")
+lines(x=(subepc$DateTime),y=subepc$Sub_metering_2,col="red")
+lines(x=(subepc$DateTime),y=subepc$Sub_metering_3,col="blue")
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="l",col=c("black","red","blue"),lwd=2,cex=0.4)
+
+plot(subepc$DateTime, subepc$Global_reactive_power, type = "l", main = "", xlab = "datetime")
+
+dev.off()
